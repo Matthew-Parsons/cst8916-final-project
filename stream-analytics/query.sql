4accf9b5130c624a35a1a1ad9f3bc14af7580707
Matthew-Parsons <pars0230@algonquinlive.com>
2025-12-05 19:08:45 -0500

    Update query.sql

--- stream-analytics/query.sql
+++ stream-analytics/query.sql
@@ -1,3 +1,4 @@
+-- Storage Account SQL
 SELECT
     IoTHub.ConnectionDeviceId AS DeviceId,
     AVG(surface_temperature) AS AvgSurfaceTemp,
@@ -18,6 +19,32 @@ SELECT
 
 INTO
     [output]
+FROM
+    [input]
+GROUP BY
+    IoTHub.ConnectionDeviceId, TumblingWindow(second, 300)
+
+-- CosmosDB SQL
+SELECT
+    IoTHub.ConnectionDeviceId AS DeviceId,
+    AVG(surface_temperature) AS AvgSurfaceTemp,
+    MIN(surface_temperature) AS MinSurfaceTemp,
+    MAX(surface_temperature) AS MaxSurfaceTemp,
+    AVG(ext_temperature) AS AvgExternalTemp,
+    MAX(snow_accumulation) AS MaxSnowAccumulation,
+    AVG(ice_thickness) AS AvgIceThickness,
+    MIN(ice_thickness) AS MinIceThickness,
+    MAX(ice_thickness) AS MaxIceThickness,
+    COUNT(*) AS ReadingCount,
+    CASE 
+        WHEN AVG(ice_thickness) >= 30 AND AVG(surface_temperature) <= -2 THEN 'Safe'
+        WHEN AVG(ice_thickness) >= 25 AND AVG(surface_temperature) <= 0 THEN 'Caution'
+        ELSE 'Unsafe'
+    END AS CurrentStatus,
+    System.Timestamp AS EventTime
+
+INTO
+    [web-output]
 FROM
     [input]
 GROUP BY
